9c472d542ea15c043e24d919cc2fdf8c86c15c38
Andrew Dmitrievsky <andrewdmitrievsky@andrews-mbp.mynetworksettings.com>
2024-07-18 22:49:48 -0500

    corrected

--- src/App.js
+++ src/App.js
@@ -7,27 +7,7 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Contact from './components/Contact';
 
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
+
 
 
 
@@ -40,14 +20,6 @@ export default function App() {
       <Projects/>
       <Skills/>
       <Contact/>
-      <div>
-      <Carousel responsive={responsive}>
-                <div style={{color:"red",}}>Item 1</div>
-                <div>Item 2</div>
-                <div>Item 3</div>
-                <div>Item 4</div>
-            </Carousel>;
-      </div>
     </div>
   );
 }
