3f948b9e2569f86fbdaa05696efafd4d6d03e808
Andrew <AndrewDmit2021@gmail.com>
2024-12-31 19:56:00 -0600

    got rid of blue hovering

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React , {useEffect,useState} from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './components/Profile';
 import Sections from './components/Sections';
 
 function App() {
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  // const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
   // Function to update cursor position based on mouse movement
-  const handleMouseMove = (e) => {
-    setCursorPosition({ x: e.clientX, y: e.clientY });
-  };
+  // const handleMouseMove = (e) => {
+  //   setCursorPosition({ x: e.clientX, y: e.clientY });
+  // };
 
   // Attach the mouse move event listener
-  useEffect(() => {
-    document.addEventListener('mousemove', handleMouseMove);
-    // Clean up the event listener when the component unmounts
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
+  // useEffect(() => {
+  //   document.addEventListener('mousemove', handleMouseMove);
+  //   // Clean up the event listener when the component unmounts
+  //   return () => {
+  //     document.removeEventListener('mousemove', handleMouseMove);
+  //   };
+  // }, []);
   
   return (
       <div className="container-fluid content-container">
-              <div
+              {/* <div
         className="custom-cursor"
         style={{
           left: `${cursorPosition.x}px`,
           top: `${cursorPosition.y}px`,
         }}
-      />
+      /> */}
         <div className='row h-100'>
           <Profile/>
           <Sections/>
